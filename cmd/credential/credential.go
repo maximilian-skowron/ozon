56b9165467fa77e03def57796377911d1a497a25
maximilian-skowron <m.skowron@mskowron.de>
2020-03-01 21:21:03 +0100

    - added comments to exported variable - changed package command

--- cmd/credential/credential.go
+++ cmd/credential/credential.go
@@ -1,9 +1,10 @@
-// Package credential
-// handler for all credential terms
+// Package credential handler for all credential terms
 package credential
 
 import "github.com/spf13/cobra"
 
+// CredentialCmd is used to create and manage credentials
+// and standard values for the different platforms
 var CredentialCmd = &cobra.Command{
 	Use:   "cred",
 	Short: "cred",
